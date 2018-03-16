473f1ff735f30cacca6a3d22a9da1e9d6dfbddc0
dears <s.ogawa211@gmail.com>
2018-03-17 01:19:58 +0900

    マルチプレクサのベース

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func main() {
+	// デフォルトのマルチプレクサ
+	mux := http.NewServeMux()
+
+	// 静的ファイルの返送
+	files := http.FileServer(http.Dir("/public")) // publicディレクトリからファイルを配信するハンドラを作成
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	mux.HandleFunc("/", index)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	files := []string{"templates/layout.html",
+		"templates/navbar.html",
+		"templates/index.html"}
+
+	templates := template.Must(template.ParseFiles(files...))
+	threads, err := data.Threads()
+	if err == nil {
+		templates.ExecuteTemplate(w, "layout", threads)
+	}
+}
